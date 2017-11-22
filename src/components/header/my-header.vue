e0a840e95ff890c961420630b9845a2e057c9c7b
zhangjc <1065453629@qq.com>
2017-11-22 16:49:26 +0800

    icon，header

---
+++ src/components/header/my-header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <div class="">推荐</div>
+    <div class="">歌手</div>
+    <div class="">排行</div>
+    <div class="">搜索</div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style lang="less" rel="stylesheet/less">
+  .header {
+    display: flex;
+    height: 50px;
+    width: 100%;
+    text-align: center;
+    line-height: 50px;
+    font-size: 16px;
+    >div{
+      flex: 1;
+    }
+  }
+</style>
